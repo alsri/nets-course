4e95a376083f62fb6c6d2ce05cad31975c9a8faa
Alina <alina@Alinas-Mac.local>
2016-02-09 17:44:57 +0100

    week 1

--- Week1/src/MyFirstThread.java
+++ Week1/src/MyFirstThread.java
@@ -1,13 +1,32 @@
 
 public class MyFirstThread extends Thread {
 	
+	public void threadPrint(String message){
+		System.out.println("Thread "+this.getId()+": "+message);
+	}
+	
 	public void printInfo(){
+		this.threadPrint("I am a new thread.");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.threadPrint("My name is "+ this.getName());
+		this.threadPrint("My priority is "+ this.getPriority());
 		
+	}
+	
+	public void run(){
+		this.printInfo();
 	}
 
 	public static void main(String[] args) {
 		
-		
+		for (int i=0;i<10;i++){
+			MyFirstThread thread=new MyFirstThread();
+			thread.start();
+		}
 
 	}
 
